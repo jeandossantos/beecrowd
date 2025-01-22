d0b268355abd330d0dc7a0818dc4e22b9293d0eb
Jean dos Santos <40217619+jeandossantos@users.noreply.github.com>
2025-01-22 20:53:21 +0000

    1017 - gasto de combustível

--- python/gasto de combustivel.py
+++ python/gasto de combustivel.py
@@ -4,6 +4,7 @@ KM_POR_L = 12
 
 tempo_gasto = int(input())
 velocidade_media = int(input())
+
 distancia_percorrida = tempo_gasto * velocidade_media
 
 litros_necessarios = distancia_percorrida / KM_POR_L
