de815cf2aa7978ca8baa796918f374eac0abaeee
Jean dos Santos <40217619+jeandossantos@users.noreply.github.com>
2025-01-22 20:52:19 +0000

    move  python files to python folder

---
+++ python/gasto de combustivel.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+KM_POR_L = 12
+
+tempo_gasto = int(input())
+velocidade_media = int(input())
+distancia_percorrida = tempo_gasto * velocidade_media
+
+litros_necessarios = distancia_percorrida / KM_POR_L
+
+print(f"{litros_necessarios:.3f}")
