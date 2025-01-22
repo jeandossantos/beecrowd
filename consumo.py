39f04de873d2e4fc4d73b32f56cbd752ebe2a744
Jean dos Santos <40217619+jeandossantos@users.noreply.github.com>
2025-01-22 20:30:27 +0000

    1014 - consumo

---
+++ consumo.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+distancia_perc = int(input())
+combustive_gasto = float(input())
+consumo_media = distancia_perc / combustive_gasto
+
+print(f"{consumo_media:.3f} km/l")
+
