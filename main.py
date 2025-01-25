d8f9a7cb427b85a67ddbe1a4b58d79f216be7a10
Jean dos Santos <40217619+jeandossantos@users.noreply.github.com>
2025-01-25 19:37:11 +0000

    1096 - Sequencia IJ 2

--- main.py
+++ main.py
@@ -1,7 +1,1 @@
-# -*- coding: utf-8 -*-
-
-i = 1
-
-for j in range(60, -1, -5):
-  print(f"I={i} J={j}")
-  i += 3
+ 
---
+++ python/sequencia_IJ_2.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+for i in range(1, 10, 2):
+  for j in [7, 6, 5]:
+    print(f"I={i} J={j}")
