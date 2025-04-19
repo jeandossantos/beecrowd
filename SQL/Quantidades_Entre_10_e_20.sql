e6a8d1c97767e5386c8da019334b141f327c2b31
Jean dos Santos <40217619+jeandossantos@users.noreply.github.com>
2025-04-19 01:17:01 +0000

    2621 - Quantidades Entre 10 e 20

---
+++ SQL/Quantidades_Entre_10_e_20.sql
@@ -0,0 +1,6 @@
+SELECT products.name AS product_name
+FROM products
+INNER JOIN providers
+ON products.id_providers = providers.id
+WHERE products.amount BETWEEN 10 AND 20
+  AND providers.name LIKE 'P%';
