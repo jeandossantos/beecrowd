2c9bbd40e4728a734596a12c2527428f39a14300
Jean dos Santos <40217619+jeandossantos@users.noreply.github.com>
2025-05-13 01:33:34 +0000

    2374 - Pneu

---
+++ ad-hoc/pneu.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+# Leitura da pressão desejada
+N = int(input())
+
+# Leitura da pressão lida pela bomba
+M = int(input())
+
+# Cálculo da diferença
+diferenca = N - M
+
+# Impressão do resultado
+print(diferenca)
